ccd6ab3d73a621f95e6a097e205a38adf791fcb6
Frederic Marcotte <frederic.marcotte.2@ulaval.ca>
2021-11-07 10:17:48 -0500

    easy input

--- graph.py
+++ graph.py
@@ -78,10 +78,13 @@ def build_json(spinor=(1,0)):
     dico_probability = local_computation(spinor, 1, G, dico_orientation)
     data = dico_orientation + dico_probability
     for n in G:
-        G.nodes[n]["display"] = str(data[n])
         if isinstance(data[n], tuple):
+            G.nodes[n]["display"] = str(data[n])
             G.nodes[n]["type"] = 0
         elif isinstance(data[n], float):
+            if data[n] < 1e-5:
+                data[n] = 0 # zero probability
+            G.nodes[n]["display"] = str(data[n])[:5] # truncate
             G.nodes[n]["type"] = 1
     # write json formatted data
     d = nx.json_graph.node_link_data(G)  # node-link format to serialize
